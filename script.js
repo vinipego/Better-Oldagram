b28e958f55b0dce12e864c85fa22e19317717b43
vinipego <vinicius.pego.pereira@gmail.com>
2025-01-06 03:26:23 -0300

    tried adding functionalities to the likes button but encountered issues
    It seems that my javascript DOM manipulation is getting in the way. It seems that what I'm trying to do would work if I weren't using firestorage. But I wanted to get firebase into this project since there wasn't a project when firebased was introduced in the course. Anyway. my console isn't even detecting simple console.log() tests even when completely outside of any function or loop.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// this console.log isn't logging. I'm not entirely sure why.
+console.log("Testing Log");
 // Import Firebase app and Firestore
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 import {
@@ -23,24 +25,46 @@ const postsRef = collection(db, "posts");
 const likeBtn = 'src="img/icon-heart.png"'; //unnecessarily used for now.
 // turn likeBtnLiked between true and false for users to like and remove their liked inside an event listener for click
 let likeBtnLiked = false; //unused
-const interactiveIcons = `
-<div class="icons-wrapper">
-    <button id="like-btn" class="icon-btn">
-        <img class="interactive-icon" ${likeBtn} alt="Heart-shapped like button."/>
+const oldagram = document.querySelector(".oldagram");
+const interactiveIcons = (docId) => `
+  <div class="icons-wrapper">
+    <button class="icon-btn like-btn" data-id="${docId}">
+      <img class="interactive-icon" src="img/icon-heart.png" alt="Heart-shaped like button" />
     </button>
     <button class="icon-btn">
-        <img class="interactive-icon" src="img/icon-comment.png" alt="dialogue-balloon-shapped comment button."/>
+      <img class="interactive-icon" src="img/icon-comment.png" alt="Comment button" />
     </button>
     <button class="icon-btn">
-        <img class="interactive-icon" src="img/icon-dm.png" alt="Paperplane-shapped share button."/>
+      <img class="interactive-icon" src="img/icon-dm.png" alt="Share button" />
     </button>
-</div>
+  </div>
 `;
+
+
+
+// Call the function to display the posts
+getPosts();
+// the DOM tries to replace the display from getPosts(); but then getPosts() overwrites it
+// oldagram.innerHTML = `<h1>Testing</h1>`;
+
+const heroHeader = document.querySelector(".hero-header");
+heroHeader.addEventListener("click", (e)=>{
+console.log("Header clicked");
+
+});
+
+oldagram.addEventListener("click", (e) => {
+    console.log("clicked");
+    const likeButton = e.target.closest(".like-btn"); // Check if the clicked element is a like button
+    if (likeButton) {
+      const postId = likeButton.dataset.id; // Retrieve the document ID from data-id
+      console.log("Liked post ID:", postId); // Log the post ID
+    }
+  });
 
 // Fetch all posts from Firestore. Async functions for the function to work asynchronously. It returns a promise and allows for the use of "await" which pauses the function until the promise is either resolved or returns an error.
 async function getPosts() {
   const querySnapshot = await getDocs(postsRef); // Fetch documents from the collection
-  const oldagram = document.querySelector(".oldagram"); // Find the container for posts
   let finalPostHTML = ``;
   //  console.log(querySnapshot.size);
   //  console.log(querySnapshot.docs);
@@ -53,29 +77,26 @@ async function getPosts() {
     // Create the HTML structure for each post
     const postHTML = `
         <section class="post">
-          <header class="post-header">
-            <img class="avatar-img" src="${post.avatar}" alt="${post.name}'s avatar">
-            <div class="name-and-location">
-              <p class="name">${post.name}</p>
-              <p class="location">${post.location}</p>
-            </div>
-          </header>
-          <figure>
-            <img class="post-img" src="${post.post}" alt="Post by ${post.name}">
-          </figure>
-          <footer class="post-footer">
-            ${interactiveIcons}
-            <p class="likes-icon"><strong>${post.likes} likes</strong></p>
-            <p><strong>${post.username}</strong> ${post.comment}</p>
-          </footer>
+            <header class="post-header">
+                <img class="avatar-img" src="${post.avatar}" alt="${post.name}'s avatar">
+                <div class="name-and-location">
+                <p class="name">${post.name}</p>
+                <p class="location">${post.location}</p>
+                </div>
+            </header>
+            <figure>
+                <img class="post-img" src="${post.post}" alt="Post by ${post.name}">
+            </figure>
+            <footer class="post-footer">
+                ${interactiveIcons(doc.id)}
+                <p class="likes-icon"><strong>${post.likes} likes</strong></p>
+                <p><strong>${post.username}</strong> ${post.comment}</p>
+            </footer>
         </section>
-      `;
-
-    // Append the post HTML to the Oldagram container
+    `;
     finalPostHTML += postHTML;
   });
   oldagram.innerHTML = finalPostHTML;
 }
 
-// Call the function to display the posts
-getPosts();
+
